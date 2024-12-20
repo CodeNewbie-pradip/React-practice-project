29ca5cfb65eb632bb5786b892e77473b9245476f
CodeNewbie-pradip <pradipalabade6442@gmail.com>
2024-12-20 22:18:44 +0530

    Completed project

---
+++ EV-olution/src/App.jsx
@@ -0,0 +1,32 @@
+import { React, useState } from 'react'
+import './App.css'
+import { Background } from './Components/Backgound/Background';
+import { Navbar } from './Components/Navbar/Navbar';
+import { Hero } from './Components/Hero/Hero';
+
+function App() {
+let heroData=[
+  {text1:"Dive into", text2:"what you love"},
+  {text1:"Indulge", text2:"your Passions"},
+  {text1:"Give in to", text2:"your passions"},
+]
+
+const [heroCount, setHeroCount]=useState(0);
+const [playStatus, setPlayStatus]=useState(false);
+
+  return (
+    <div>
+    <Background playStatus={playStatus} heroCount={heroCount}/>
+    <Navbar/>
+    <Hero 
+    setPlayStatus={setPlayStatus}
+    heroData={heroData[heroCount]}
+    heroCount={heroCount}
+    setHeroCount={setHeroCount}
+    playStatus={playStatus}
+    />
+    </div>
+  )
+}
+
+export default App
