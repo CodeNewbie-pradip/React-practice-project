e9ea566f0c5f2394dc93280fa20eefd3681ef2f8
CodeNewbie-pradip <pradipalabade6442@gmail.com>
2024-12-23 13:04:54 +0530

    completed brand page

---
+++ Brand Page/src/App.jsx
@@ -0,0 +1,15 @@
+import Navigation from './Components/Navigation';
+import './App.css'
+import HeroSection from './Components/Hero';
+
+const App=()=> {
+
+  return (
+    <div>
+      <Navigation/>
+      <HeroSection/>
+    </div>
+  )
+}
+ 
+export default App;
